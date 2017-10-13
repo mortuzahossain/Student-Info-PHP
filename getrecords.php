fc0d7bcbb73b474da12167c96aba236824883b10
Mortuza Hossain <mortuzahossain1997@gmail.com>
2017-10-13 23:14:13 +0600

    Added Some Change in SQL query

--- getrecords.php
+++ getrecords.php
@@ -5,6 +5,6 @@ include 'db_connect.php';
 $limit = (intval($_GET['limit']) != 0 ) ? $_GET['limit'] : 10;
 $offset = (intval($_GET['offset']) != 0 ) ? $_GET['offset'] : 0;
 
-$sql = "SELECT * FROM student_info WHERE id ORDER BY id ASC LIMIT $limit OFFSET $offset";
+$sql = "SELECT * FROM student_info WHERE 1 ORDER BY id ASC LIMIT $limit OFFSET $offset";
 show_data($sql,$con);
 ?>
